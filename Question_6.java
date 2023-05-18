6985453ff09b655e5f7745953f8dc9822b1b7538
u2100687 <u2100687@siswa365.um.edu.my>
2023-05-18 11:42:07 +0800

    Rename file to Q6

--- Question_6.java
+++ Question_6.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class Question_6 {
     public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Text: ");
-    String Text = sc.nextLine();
+        //Ask user to enter text
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Text: ");
+        String Text = sc.nextLine();
+        System.out.print("Shift: ");
+        int Shift = sc.nextInt();
 
-    System.out.print("Shift: ");
-    int Shift = sc.nextInt();
-
-    Decryption(Text, Shift);
-}
-public static void Decryption(String Text, int Shift) {
+        Decryption(Text, Shift);
+    }
+    public static void Decryption(String Text, int Shift) {
         String decrypted;
         ArrayList<String> List = new ArrayList<>();
         String Alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -20,43 +20,63 @@ public static void Decryption(String Text, int Shift) {
         String[] Letter = Text.split("");
         //Shift alphabet and space '$'
         for (int i = 0; i < Letter.length; i++) {
-        for (int j = 0; j < Alpha.length; j++) {
-        if (Letter[i].equalsIgnoreCase(Alpha[j])) {
-        int num = j-Shift;
-        if(num<0){
-        num =26+num;
-        }
-        decrypted = Alpha[num];
-        List.add(Alpha[num]);
-        }
-        else  {
-        if (Letter[i].equals("$")) {
-        List.add(" ");
-        break;
-        } else if (Letter[i].equals("^")) {
-        List.add("^");
-        break;
-        }
-        else{
-        List.add(Letter[i]);
-        break;
-        }
-        }
+            for (int j = 0; j < Alpha.length; j++) {
+                if (Letter[i].equalsIgnoreCase(Alpha[j])) {
+                    int num = j - Shift;
+                    if (num < 0) {
+                        num = 26 + num;
+                    }
+                    decrypted = Alpha[num];
+                    List.add(decrypted);
+                }
+                if (Letter[i].equals("$")) {
+                    List.add(" ");
+                    break;
+                } else if (Letter[i].equals("^")) {
+                    List.add("^");
+                    break;
+                } else if (Letter[i].equals(",")|Letter[i].equals(".")|Letter[i].equals("(")|Letter[i].equals(")")) {
+                    List.add(Letter[i]);
+                    break;
+                }
+            }
 
-        }
         }
         //Capitalized '^'
         for (int i = 0; i < List.size() - 1; i++) {
-        if (List.get(i).equals("^")) {
-        String nextElement = List.get(i+1);
-        String updatedElement = nextElement.toUpperCase();
-        List.set(i+1, updatedElement);
-        List.remove(i);
+            if (List.get(i).equals("^")) {
+                String nextElement = List.get(i+1);
+                String updatedElement = nextElement.toUpperCase();
+                List.set(i+1, updatedElement);
+                List.remove(i);
+            }
         }
+
+        ArrayList<String> Word = new ArrayList<>();
+        for (int i = 0; i < List.size(); i++) {
+            Word.clear();
+            if(List.get(i).equals("(")){
+                for (int j = i+1;; j++) {
+                    Word.add(List.get(j));
+                    if(List.get(j).equals(")")) {
+                        for (int k = Word.size()-1; k >= 0; k--) {
+                            List.set(i++, Word.get(k));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < List.size(); i++) {
+
         }
 
         //Print out
-        for (int i = 0; i < List.size(); i++)
-        System.out.print(List.get(i));
+        for (int i = 0; i < List.size(); i++) {
+            if (List.get(i).equals(")")) {
+                List.remove(i);
+            }
+            System.out.print(List.get(i));
         }
-        }
+    }
+}
