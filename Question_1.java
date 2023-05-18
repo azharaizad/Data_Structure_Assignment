020509377159da63edd4c0d3f2c3e406fb0741be
u2100687 <u2100687@siswa365.um.edu.my>
2023-05-18 09:16:56 +0800

    Q6

--- Question_1.java
+++ Question_1.java
@@ -1,5 +1,70 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Question_1 {
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        //Ask user to enter text
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Text: ");
+        String Text = sc.nextLine();
+
+        System.out.print("Shift: ");
+        int Shift = sc.nextInt();
+
+        Decryption(Text, Shift);
+    }
+
+    public static void Decryption(String Text, int Shift) {
+        String decrypted;
+        ArrayList<String> List = new ArrayList<>();
+        String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String[] Alpha = Alphabet.split("");
+        String[] Letter = Text.split("");
+
+        //Shift alphabet and space '$'
+        for (int i = 0; i < Letter.length; i++) {
+            for (int j = 0; j < Alpha.length; j++) {
+                if (Letter[i].equalsIgnoreCase(Alpha[j])) {
+                    int num = j-Shift;
+                    if(num<0){
+                        num =26+num;
+                    }
+                    decrypted = Alpha[num];
+                    //System.out.print(decrypted);
+                    List.add(Alpha[num]);
+                }
+                else  {
+                    if (Letter[i].equals("$")) {
+                        //System.out.print(" ");
+                        List.add(" ");
+                        break;
+                    } else if (Letter[i].equals("^")) {
+                        //System.out.print(" ");
+                        List.add("^");
+                        break;
+                    }
+                    else{
+                        List.add(Letter[i]);
+                        break;
+                    }
+                }
+
+                }
+            }
+        //Capitalized '^'
+        for (int i = 0; i < List.size() - 1; i++) {
+            if (List.get(i).equals("^")) {
+                String nextElement = List.get(i+1);
+                String updatedElement = nextElement.toUpperCase();
+                List.set(i+1, updatedElement);
+                List.remove(i);
+            }
+        }
+
+        //Print out
+        for (int i = 0; i < List.size(); i++)
+            System.out.print(List.get(i));
     }
 }
