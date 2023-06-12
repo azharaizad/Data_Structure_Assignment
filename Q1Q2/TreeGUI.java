8e992e966bd2dfb622adf0c2b2df7e12aeca5ac1
pfizril <98295726+pfizril@users.noreply.github.com>
2023-06-12 22:38:37 +0800

    TreeGUI class

---
+++ Q1Q2/TreeGUI.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
+
+public class TreeGUI extends JFrame {
+    private JTree tree;
+
+    public TreeGUI(DefaultMutableTreeNode rootNode) {
+        super("General Tree");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(400, 300));
+
+        tree = new JTree(rootNode);
+        JScrollPane scrollPane = new JScrollPane(tree);
+
+        getContentPane().add(scrollPane);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
